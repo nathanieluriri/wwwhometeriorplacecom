7e11bfc56b5f3a00134ea4b50a2b7d05910da119
nathanieluriri <95766173+nathanieluriri@users.noreply.github.com>
2023-07-02 01:23:35 +0100

    contact looks somehow better

--- views/contact.views.php
+++ views/contact.views.php
@@ -6,15 +6,23 @@
 <?php require("views/partials/nav.php") ?>
 
 <?php require("views/partials/hero.php") ?>
-    <section class="content-wrapper"><div class="info-section"><h2>Ask for our help <a href="https://wa.link/tg5cfj"><button class="btn">Request a Consultation</button></a></h2> <p>Request a personalized interior design for your apartment</p> <p>In order to craft your dream interior design, we kindly request that you provide us with some essential information. By sharing these details, we can tailor our services to meet your specific needs and preferences, ensuring a truly personalized experience.</p></div> 
+    <section class="content-wrapper ContactUs">
+        <div class="info-section">
+        <h2>Ask for our help <a target="_blank" href="https://wa.link/tg5cfj">
+        <button class="btn">Request a Consultation via WhatsApp</button></a></h2> 
+        <p>Request a personalized interior design for your apartment</p>
+         <p>In order to craft your dream interior design, we kindly request that you provide us with some essential information. By sharing these details, we can tailor our services to meet your specific needs and preferences, ensuring a truly personalized experience.</p>
+        </div> 
+
+
         <div class="contact-section">
             <form action="mailto:ContactUs@hometerior.com" method="post" enctype="text/plain">
-                <label >Name:</label>
+                <label for="name" >Name:</label>
                 <input type="text" name="name"><br> <br>
                 <label>E-mail:</label>
-                <input type="text" name="mail"><br> <br>
+                <input type="email" name="mail"><br> <br>
                 <label >Message:</label>
-                <textarea> q </textarea><br><br>
+                <textarea>  </textarea><br><br>
                 <i class="fas fa-check-circle "></i>
                 <i class="fas fa-exclamation-circle "></i>
                 <input class="btn" type="submit" value="Send">
