d53b3149d5a25bee9ce12d7d0caec7787e0d87a3
nathanieluriri <95766173+nathanieluriri@users.noreply.github.com>
2023-08-16 12:25:13 +0100

    ready for sammy

--- debug.try.php
+++ debug.try.php
@@ -5,7 +5,7 @@
 <input type="text" name="surname" id="Try-text">
 <input type="submit" value="submit">
 </form>
-
+<div style="padding-top:75.000%;position:relative;"><iframe src="https://gifer.com/embed/758b" width="100%" height="100%" style='position:absolute;top:0;left:0;' frameBorder="0" allowFullScreen></iframe></div>
 <pre></pre>
 <?php
 $name=$_POST["name"];
